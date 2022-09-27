2895358dd890438cbfa62b53e9e2030390bf037e
Ilnur37 <dinmukhameto@mail.ru>
2022-09-27 12:59:11 +0300

    Java-Based

---
+++ src/main/java/com/dev/spring/listner/entity/AccessType.java
@@ -0,0 +1,5 @@
+package com.dev.spring.listner.entity;
+
+public enum AccessType {
+    CREATE, UPDATE, READ, DELETE
+}
---
+++ src/main/java/com/dev/spring/listner/entity/EntityListener.java
@@ -0,0 +1,13 @@
+package com.dev.spring.listner.entity;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntityListener {
+
+    @EventListener
+    public void acceptEntity (EntityEvent entityEvent) {
+        System.out.println("Entity" + entityEvent);
+    }
+}
